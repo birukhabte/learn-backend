7274191480c55c1f95366e0b6dce0d56448bd09e
biruk habte <biruk.habte-ug@aau.edu.et>
2026-03-02 11:45:33 +0300

    connected to database

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,1 +1,15 @@
 import mongoose from "mongoose"
+
+const connectDB = async () =>{
+    try{
+        const connectionInstance = await mongoose.connect
+        (`${process.env.MONGODB_URI}`)
+        console.log(`/ln MongoDb connected !!!
+           ${connectionInstance.connection.host} `)
+    } catch (error){
+     console.log("MongoDb connection failed",error);
+     process.exit(1)
+    }
+}
+
+export default connectDB;
