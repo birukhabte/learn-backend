2a4340af640432fb6426683809c2d1511198e088
biruk habte <biruk.habte-ug@aau.edu.et>
2026-03-03 10:03:06 +0300

    Configure server startup, add MongoDB connection error handling, and define server port.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,19 +3,21 @@ import dotenv from "dotenv";
 import connectDb from '.config/database.js';
 
 dotenv.config({
-path:"./.env"
+    path: "./.env"
 });
-const startServer =async()=>{
-    try{
+const startServer = async () => {
+    try {
         await connectDB();
-        app.on('error',(error)=>{
-            console.log("ERROR",error);
+        app.on('error', (error) => {
+            console.log("ERROR", error);
             throw error;
         });
-        app.listen(process.env.PORT || 8000,()=>{
+        app.listen(process.env.PORT || 8000, () => {
             console.log(`Server is running on port ${process.env.PORT}`);
         })
     }
- catch (error){
-
- }}
+    catch (error) {
+        console.log("MongoDb connection failed !!", error)
+    }
+}
+startServer();
