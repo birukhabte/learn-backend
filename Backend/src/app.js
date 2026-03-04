bbff8680266d44a935685d43c118455930c6f6ef
biruk habte <biruk.habte-ug@aau.edu.et>
2026-03-04 11:31:04 +0300

    Add JSON body parsing middleware and integrate user routes for `/api/v1/users`.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,16 @@
 
 import express from "express"
 
+app.use(express.json());
+
 const app =express(); // create an express app
+
+// routes import
+ import userRouter from './routes/user.route.js'
+
+ // routes declaration
+ app.use('/api/v1/users',userRouter)
+
+// example route: http://localhost:4000/api/v1/users/register
 
 export default app;
