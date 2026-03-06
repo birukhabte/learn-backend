11ca085a325934d2c321e368f1fe094cf66116c7
biruk habte <biruk.habte-ug@aau.edu.et>
2026-03-06 11:03:10 +0300

    Implement user login functionality, fix registration email typo, and update server port.

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 
-import { user } from "../models/user.model.js";
+import { User } from "../models/user.model.js";
 
 const registerUser = async (req, res) => {
     try {
@@ -15,7 +15,7 @@ const registerUser = async (req, res) => {
         // check if user exists already
 
         const existing = await User.findOne({
-            eamil: email.toLowerCase()
+            email: email.toLowerCase()
         });
         if (existing) {
             return res.status(400).json({
@@ -41,6 +41,33 @@ const registerUser = async (req, res) => {
 
     }
 
+}
+
+const loginUser = async (req,res)=>{
+    try{
+        //check if the user already exists
+        const {email,password}=req.body;
+
+        const user = await User.findOne({email:email.toLowerCase()});
+          email:email.toLowerCase()
+
+          if(!user){
+            return res.status(404).json({
+                message:"User not found"
+            })
+          }
+          if(user.password !== password){
+            return res.status(401).json({
+                message:"Invalid password"
+            })
+          }
+          user.loggedIn = true;
+          await user.save();
+          return res.status(200).json({
+            message:"User logged in successfully",
+            user: { id: user._id, email: user.email, username: user.username }
+          })
+    }
 }
 
 export {
